6d51c3040a686a432e59041cf9291f6df8979c3a
Richard Spencer <richardjspencer93@gmail.com>
2018-02-08 09:56:48 +0000

    Seperated preprocessing logic added bool options Options added to disable certain preprocess steps leaving the image closer to the input image Updated matching demo to utilise this, as internsity rescaling can disrupt results

--- matching_demo.py
+++ matching_demo.py
@@ -3,8 +3,6 @@ from contextlib import suppress
 import os
 import json
 from PIL import Image
-from PIL import ImageChops
-from random import sample
 
 glyph_directory = './Glyphs'
 
@@ -22,20 +20,6 @@ for filename in os.listdir(glyph_directory):
         image = Image.open(path)
         glyphs.update({name: image})
 
-glyph_list = list(glyphs.items())
-glyph_list_sample = sample(glyph_list, 20)
-
-output = Image.new("L", (27*25, 24*48))
-for i in range(1, 26):
-    art = art_typing.ArtTyping(glyphs, glyph_depth=1, samples=(i, min(i*2, 48)))
-    for j, (name, image)in enumerate(glyph_list_sample):
-        calc, out, ins = art.image_to_text(image, target_size=(1, 1), cutoff=0)
-        output.paste(out, ((i-1)*25, (j+2)*48, i*25, (j+3)*48))
-
-for j, (name, image)in enumerate(glyph_list_sample):
-    output.paste(image, (26*25, (j+2)*48, 27*25, (j+3)*48))
-
-
 # Input images are glyph images
 # Our code should match these perfectly with the same glyph
 # This is only guaranteed at samples=(25, 48)
@@ -49,11 +33,9 @@ for j, (name, image)in enumerate(glyph_list_sample):
 # self.value_extrema = (150, 250)
 # certainly improves reproduction, if the rescale_intensity is present
 
-# Something in the rescaling intensity is throwing it off, and making • overly present
-output.show()
+# rescale_intensity is disables, as it throws off the matching, and makes • overly present
 
-# I suppose this is reasonable. We are adjusting a single glyph image to match the full range
-# Perhaps a better test would be to take the full set of glyphs, and do this
+glyph_list = list(glyphs.items())
 
 column_image = Image.new("L", (25, 48*(len(glyph_list))))
 for j, (name, image)in enumerate(glyph_list):
@@ -62,7 +44,8 @@ for j, (name, image)in enumerate(glyph_list):
 output = Image.new("L", (25*27, 48*(len(glyph_list)+2)))
 for i in range(1, 26):
     art = art_typing.ArtTyping(glyphs, glyph_depth=1, samples=(i, min(i*2, 48)))
-    calc, out, ins = art.image_to_text(column_image, target_size=(1, len(glyph_list)), cutoff=0)
+    calc, out, ins = art.image_to_text(column_image, target_size=(1, len(glyph_list)), cutoff=0,
+                                       use_clahe=False, rescale_intensity=False)
     output.paste(out, ((i-1)*25, 48*2, i*25, 48*(len(glyph_list)+2)))
 
 output.paste(column_image, (26*25, 48*2, 27*25, 48*(len(glyph_list)+2)))
