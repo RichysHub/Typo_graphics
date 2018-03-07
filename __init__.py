6f3b1380271fcca0c40d49abb597ce5a385adc85
Richard Spencer <richardjspencer93@gmail.com>
2018-03-07 01:16:20 +0000

    Typograph import from glyph directory by default Typograph now can now have no glyphs passed, which will use the default set. Also cleaned up imports in a few places

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
-from .typograph import Typograph
+import os
 from .glyph import Glyph
+from .typograph import Typograph
+
+__all__ = [Typograph, Glyph]
+package_directory = os.path.dirname(os.path.abspath(__file__))
