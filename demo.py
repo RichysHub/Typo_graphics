0ab74164ebe81f57d0e695b8b567f3c77447c4a1
Richard Spencer <richardjspencer93@gmail.com>
2018-03-14 11:48:46 +0000

    Added docstring for _scale_to_max_size Fixed some minor docstrings elsewhere changed default cutoff to zero

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ typograph = Typograph(glyph_depth=2)
 
 # convert the input image into an image composed of glyphs
 target = Image.open('./dog.png')
-calc, out, ins = typograph.image_to_text(target, cutoff=0)
+calc, out, ins = typograph.image_to_text(target)
 
 # display the output that the instructions would render
 out.show()
